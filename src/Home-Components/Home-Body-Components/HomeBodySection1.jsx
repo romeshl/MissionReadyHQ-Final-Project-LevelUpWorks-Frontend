878747bedcb3a6da2fda5ae39f0f5f138d2893e0
Romesh Lankathilaka <romeshl@missionreadyhq.com>
2024-03-17 22:02:29 +1300

    Home Page -> Body, Decided to divide the body into 5 sections and build seperate components for each section. Section 1 is complete.

---
+++ src/Home-Components/Home-Body-Components/HomeBodySection1.jsx
@@ -0,0 +1,42 @@
+import Style from "./HomeBodySection1.module.css";
+
+export default function HomeBodySection1() {
+    return (
+        <>
+            {/* This is the Section-1 of the body */}
+            <div className={Style.bodySection1Div}>
+
+                {/* This div holds the 2 text elements and buttons */}
+                <div className={Style.bodySection1DivElements}>
+                    {/* First text element */}
+                    <p className={Style.bodySection1P}>
+                        Prepare young minds for a better{" "}
+                        <span className={Style.bodySection1Span}>future.</span>
+                    </p>
+                    {/* Second text element */}
+                    <p className={Style.bodySection1P2}>
+                        Let us help you advance students in Digital Technologies and other
+                        learning areas with our project-based learning programme.
+                    </p>
+                    {/* This Div holds the buttons and the small print text */}
+                    <div className={Style.bodySection1ButtonsDiv}>
+                        {/* Learn more button */}
+                        <button className={Style.bodySection1ButtonLearnMore}>
+                            LEARN MORE
+                        </button>
+                        {/* This Div holds the 'Sign up' button and small print text */}
+                        <div className={Style.bodySection1SignupButtonDiv}>
+                            {/* Sign up button */}
+                            <button
+                                className={Style.bodySection1ButtonSignup}
+                                onClick={() => alert("Ok")}>SIGN UP</button>
+                            {/* Small print text */}
+                            *Basic subscription includes the first 15 projects free
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/*------------Section-1 end ------------*/}
+        </>
+    );
+}
--- src/Home-Components/HomeBody.jsx
+++ src/Home-Components/HomeBody.jsx
@@ -1,34 +1,9 @@
-import Style from "./HomeBody.module.css";
+import HomeBodySection1 from "./Home-Body-Components/HomeBodySection1"
 
 export default function HomeBody() {
-    function text() {
-        alert("ok");
-    }
-    return (
+  return (
     <>
-      <div className={Style.bodySection1Div}>
-        <div className={Style.bodySection1DivText}>
-          <p className={Style.bodySection1P}>
-            Prepare young minds for a better{" "}
-            <span className={Style.bodySection1Span}>future.</span>
-          </p>
-          <p className={Style.bodySection1P2}>
-            Let us help you advance students in Digital Technologies and other
-            learning areas with our project-based learning programme.
-          </p>
-          <div className={Style.bodySection1ButtonsDiv}>
-            <button className={Style.bodySection1ButtonLearnMore}>
-              LEARN MORE
-            </button>
-            <div className={Style.bodySection1SignupButtonDiv}>
-              <button className={Style.bodySection1ButtonSignup} onClick={()=>alert("Ok")}>
-                SIGN UP
-              </button>
-              *Basic subscription includes the first 15 projects free
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+      <HomeBodySection1/>
+      </>
+)
 }
